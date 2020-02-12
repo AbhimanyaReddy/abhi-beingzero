5f8fe3427f0e16fbcb65908b027679627ab0a2d4
AbhimanyaReddy <41120815+AbhimanyaReddy@users.noreply.github.com>
2020-02-12 09:55:06 +0530

    Update site.js

--- frontend/JS/site.js
+++ frontend/JS/site.js
@@ -1,6 +1,7 @@
 
 $.getJSON('/data', function(d){
     //alert(JSON.stringify(d));
+    alert("HELLO");
     $('#name').text(d.name);
     $('#college').text(d.college);
     $('#regno').text(d.regno);
